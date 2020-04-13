9e3d87734977b4140a5d731e80fac305ac218718
erpika <58664164+erpika@users.noreply.github.com>
2020-04-13 17:20:57 +0530

    Update adm_letter.sh

--- adm_letter.sh
+++ adm_letter.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+
 # Script to generate addmison letters
 # Read CSV file
 # Save each column in separate array
 # Generate text file as per template by replacing specific fields from array
+
+# Testing with single entry then will put in loop
+# Saving values in different variables
+sno="1"
+name="John"
+city="NY"
+hno="121L"
+rollNo="1001"
+email="john\@ gmail.com"
+mobile="1234567890"
+filename="${name}_${rollno}"
+# Write varaible to a new text file along with template text
