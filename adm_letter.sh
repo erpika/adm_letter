a7b7d3147ef327fd2bd596f1c7df0863d05775a4
erpika <58664164+erpika@users.noreply.github.com>
2020-04-13 17:47:59 +0530

    Update adm_letter.sh

--- adm_letter.sh
+++ adm_letter.sh
@@ -11,8 +11,32 @@ sno="1"
 name="John"
 city="NY"
 hno="121L"
-rollNo="1001"
+rollno="1001"
 email="john\@ gmail.com"
 mobile="1234567890"
 filename="${name}_${rollno}"
+date=$(date +'%m/%d/%Y')
 # Write varaible to a new text file along with template text
+if [ ! -e ${filename} ]; then
+  echo "Date: $date  No. $sno
+
+To
+
+$name
+$hno
+$city
+
+Dear $name,
+
+Greeating of the day,
+
+We have processed your application, and you have been alloted $rollno as your Class Roll No.
+
+Same information has been sent on @email and as SMS on $mobile.
+
+Please acknowledge the receipt of this letter.
+
+Yours truly,
+
+Registrar" >> ${filename}.txt
+fi
